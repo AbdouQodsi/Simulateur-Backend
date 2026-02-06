c0b0288b29b759bac8ea640dbd527310e14e58d9
Dev-digistricom <abdellah.digistricom@gmail.com>
2026-02-06 23:15:16 +0100

    CORS configuration

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,9 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
     app.enableCors({
-    origin: 'http://localhost:3000', // your Next.js frontend
-    credentials: true, // if you need cookies
-  });
+        origin: true,
+        credentials: true,
+      });
 
   await app.listen(process.env.PORT ?? 3001);
 }
